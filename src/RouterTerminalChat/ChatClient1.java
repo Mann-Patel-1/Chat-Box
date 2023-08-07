9f3fe83b8a2fc9e6cd16d75187f20e4edfc9fee2
mannpatel2 <mann.patel1@ucalgary.ca>
2023-08-06 22:42:50 -0600

    Final Commit

---
+++ src/RouterTerminalChat/ChatClient1.java
@@ -0,0 +1,54 @@
+package TerminalChat;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatClient1 {
+    private static final String SERVER_IP = "2001:56a:7950:fa00:d89a:67e7:c467:1e9e";
+    private static final int SERVER_PORT = 8080;
+    private String username;
+
+    public ChatClient1(String username) {
+        this.username = username;
+    }
+
+    public void start() {
+        try {
+            Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+            Thread messageReceiverThread = new Thread(() -> {
+                try {
+                    Scanner reader = new Scanner(socket.getInputStream());
+                    while (reader.hasNextLine()) {
+                        String message = reader.nextLine();
+                        System.out.println(message);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            messageReceiverThread.start();
+
+            Scanner userInput = new Scanner(System.in);
+            while (true) {
+                String message = userInput.nextLine();
+                writer.println(username + ": " + message);
+                writer.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter your username:");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+
+        ChatClient1 client = new ChatClient1(username);
+        client.start();
+    }
+}
---
+++ src/TerminalChat/ChatServer.java
@@ -0,0 +1,82 @@
+package TerminalChat;
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatServer {
+    private List<PrintWriter> clients = new ArrayList<>();
+
+    public static void main(String[] args) {
+        new ChatServer().startServer();
+    }
+
+    public void startServer() {
+        try (ServerSocket serverSocket = new ServerSocket(12345)) {
+            System.out.println("Chat Server started. Listening for connections...");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                clientSocket.getInetAddress();
+                System.out.println("New client connected: " + clientSocket.getInetAddress());
+
+                // Create a new thread to handle client communication
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clients.add(clientHandler.getWriter());
+                new Thread(clientHandler).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Broadcast message to all connected clients
+    public void broadcastMessage(String message) {
+        for (PrintWriter client : clients) {
+            client.println(message);
+            client.flush();
+        }
+    }
+
+    private class ClientHandler implements Runnable {
+        private Socket clientSocket;
+        private BufferedReader reader;
+        private PrintWriter writer;
+
+        public ClientHandler(Socket socket) throws IOException {
+            this.clientSocket = socket;
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(socket.getOutputStream(), true);
+        }
+
+        public PrintWriter getWriter() {
+            return writer;
+        }
+
+        @Override
+        public void run() {
+            String username = null;
+            try {
+                // Get the username from the client
+                username = reader.readLine();
+                broadcastMessage(username + " joined the chat.");
+
+                String message;
+                while ((message = reader.readLine()) != null) {
+                    broadcastMessage(username + ": " + message);
+                }
+            } catch (IOException e) {
+                System.out.println(username + " left the chat.");
+            } finally {
+                try {
+                    clientSocket.close();
+                    clients.remove(writer);
+                    broadcastMessage(username + " left the chat.");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
